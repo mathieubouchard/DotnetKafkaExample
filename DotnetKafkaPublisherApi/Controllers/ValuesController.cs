685a7eb3c410bd30ebdb1a9acafd6696da09f73a
Mathieu Bouchard <mbouchar@gmail.com>
2019-07-11 19:12:45 -0400

    Remove comments

--- DotnetKafkaPublisherApi/Controllers/ValuesController.cs
+++ DotnetKafkaPublisherApi/Controllers/ValuesController.cs
@@ -12,20 +12,6 @@ namespace DotnetKafkaPublisherApi.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
-/*        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        } */
-
         // POST api/values
         [HttpPost]
         public string Post([FromBody] string value)
@@ -51,17 +37,5 @@ namespace DotnetKafkaPublisherApi.Controllers
 
             return "Succès";
         }
-
-        // PUT api/values/5
-/*        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        } */
-
-        // DELETE api/values/5
-/*        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        } */
     }
 }
